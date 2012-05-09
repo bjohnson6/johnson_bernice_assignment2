bdb67cbc02ec9845d101ca48f6b96b5868d1ac94
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-08 23:52:12 -0400

    Added all the flowchart elements as comments.

--- johnson_bernice_assignment2/johnson_bernice_assignment2.js
+++ johnson_bernice_assignment2/johnson_bernice_assignment2.js
@@ -1,11 +1,59 @@
-//Bernice Johnson
-//May 6, 2012
-//Deliverable 2
-// Getting my daughter ready for school.
+ //Bernice Johnson
+ //May 6, 2012
+ //Deliverable 2
+ // Getting my daughter Emma ready for school in the morning.
+
+//alert("JavaScript works!");
+
+  //Initial Variables
+
+  var  string
+  var  number
+  var  array
+
+  // Procedure
+
+  Procedure /argument
+   argument conditional  = true  = output then back
+   argument conditional  = false = output then back
 
 
 
+  // Boolean Function
+
+  Boolean Function/ argument/argument
+   Comparison = true = output / return Boolean
+   Comparison = false = output / return Boolean
 
 
+  // Number Function
 
-alert("JavaScript works!");
+  Number Function/ Number
+   local Variables
+   while Loop = true / Math = output
+   while Loop = false / return number
+
+
+  //String Funtion
+  
+  String Function / string/string 
+   Local variables 
+   String Concatenation
+   return string 
+
+  //Array Function
+
+  Array Function / Number / Array
+   local variables 
+   For loop = true = math = output
+   For loop = false = return array
+
+  //Returned Values
+  //Output
+  //Finish 
+
+
+ 
+  
+
+
