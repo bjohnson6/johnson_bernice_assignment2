e2bc098e4730d7014a6f27ab740e68f516458e86
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-05-10 19:36:12 -0400

    I finished the boolean function and number function and still trying to fix my procedure. I also started the String function.

--- johnson_bernice_assignment2/johnson_bernice_assignment2.js
+++ johnson_bernice_assignment2/johnson_bernice_assignment2.js
@@ -7,39 +7,85 @@
 
   //Initial Variables
 
-  var  string
-  var  number
-  var  array
+  var      wakeUP  = "swipe off the alarm",
+  var       getUp  =  8,
+  var    daughter  = "Emma",
+  var        late  = "stay in bed",
+  var       myDog  = "Mookie",
+  var     weather  =  70,
+  var   StuffToDo  = ["brush teeth", "get dressed", "check homework", "eat breakfast"],
 
   // Procedure
 
-  Procedure /argument
-   argument conditional  = true  = output then back
-   argument conditional  = false = output then back
+  var alarmClock = function (wakeUp) {
+     console.log ("What is that sound I hear telling me to wake up?");
+ };     var alarmSays = 8 < 9 
+    
+
+      if (alarmSays === true) {
+      	var daughter = "Emma";
+      	 console.log(" It's 8 am, I need to " + wakeUp +" and get out of bed and wake up " + daughter + ".");
+               alarmClock("swipe off the snooze button")
+       }else{
+       	var late ="stay in bed"
+            console.log(WakeUp + late + "and be late for school.");
+    };
+            alarmClock("swipe off the snooze button")
+
+  
+  
 
 
 
-  // Boolean Function
 
-  Boolean Function/ argument/argument
-   Comparison = true = output / return Boolean
-   Comparison = false = output / return Boolean
+  //Procedure /argument
+   //argument conditional  = true  = output then back
+   //argument conditional  = false = output then back
 
 
-  // Number Function
 
-  Number Function/ Number
-   local Variables
-   while Loop = true / Math = output
-   while Loop = false / return number
+  // Boolean Function //use a boolean expression
+var GetReady = function (name, clothes) {
+	  var whatToWear = ( name && clothes);
+      var weather = 70 > 80
+	if (weather ===true) {
+		console.log ( name + "will wear a" + clothes + " and flipflops for school.");
+		
+		var kids = 7>3 
+		if (kids ===true) {
+		console.log ("We are all ready to head down stairs now. Emma will watch her scoobydoo movies and " + myDog + " will wait on his mat while I get them some breakfast.");
+          ready = whatToWear;
+          return ready;
+	}else{
+      console.log (name + " will wear a cute shirt and pants.");
+     };
+     ready = whatToWear;
+     return ready;
+ };
+     
+     GetReady("Emma", "spring dress");
+     
+	
+}
+  //Boolean Function/ argument/argument
+  // Comparison = true = output / return Boolean
+  // Comparison = false = output / return Boolean
+
+
+  
 
 
   //String Funtion
   
-  String Function / string/string 
-   Local variables 
-   String Concatenation
-   return string 
+   var breakfast = function(food, drink){
+        var e = "Emma";
+        var m = "mookie";
+
+   }
+  //String Function / string/string 
+   //Local variables 
+  // String Concatenation
+  // return string 
 
   //Array Function
 
@@ -47,6 +93,28 @@
    local variables 
    For loop = true = math = output
    For loop = false = return array
+
+
+
+   // Number Function
+
+    var timeTillSchool = function(number) {
+    	var minutesTillSchool = 30
+    	var timeToGetThere = 10      //add i
+    	while (minutesTillSchool > timeToGetThere) {
+    		console.log( "Time to go to school, it takes " + timeToGetThere + " minutes to drive there and school starts in " + minutesTillSchool + " minutes");
+    		return minutesToSchool-timeToGetThere;
+    };
+     console.log ("We have" + minutesTillSchool + "minutes left until school starts.") ;
+     return minutesTillSchool;		
+     timeTillSchool(20); 
+
+    	};
+    
+  //Number Function/ Number
+   //local Variables
+   //while Loop = true / Math = output
+  // while Loop = false / return number
 
   //Returned Values
   //Output
